de64833f93d3cc9568e93cbc5ed3b0f39d77ee3a
Татьяна <yuki-shuichi@mail.ru>
2022-08-31 18:45:02 +0300

    Первая задача

---
+++ Task#25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int c = 1;
+
+while (b <= 0){
+    Console.WriteLine("Введите натуральное число: ");
+    b = int.Parse(Console.ReadLine());
+}
+
+while (b >= 1){
+    c *= a;
+    b = b-1;
+}
+
+Console.WriteLine("ОТВЕТ: " + c);
